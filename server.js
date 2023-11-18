15f3237ed23cf924c1ca2c6cbe90ff3385527acd
Oleksandr Miziuk <gtmizyuk@gmail.com>
2023-11-18 12:10:56 +0200

    add custom routes in server.js

--- server.js
+++ server.js
@@ -6,7 +6,18 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares)
+server.use(jsonServer.rewriter({
+  "/api/*": "/$1",
+  "/mysteries/:id": "/mysteries/:id",
+  "/mysteries/title/:title": "/mysteries?title=:title",
+  "/mysteries/mission-id-name/:id/:name": "/mysteries?missions[:id].name=:name",
+  "/mysteries/mission-id-date/:id/:date": "/mysteries?missions[:id].date=:date",
+  "/mysteries/location/:location": "/mysteries?location=:location",
+  "/mysteries/found/:found": "/mysteries?found=:found",
+  "/mysteries/mysterium/:mysterium": "/mysteries?q=:mysterium",
+  "/mysteries/explanation/:explanation": "/mysteries?q=:explanation"  
+}))
 server.use(router)
 server.listen(port, () => {
-  console.log('JSON Server is running')
+  console.log('JSON-сервер працює!')
 })
