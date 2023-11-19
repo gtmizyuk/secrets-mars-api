98bf2a61fe32396fcccc86fe1539bae415cf1d61
Oleksandr Miziuk <gtmizyuk@gmail.com>
2023-11-19 12:05:17 +0200

    add custom routes

--- server.js
+++ server.js
@@ -10,14 +10,15 @@ const port = process.env.PORT || 3000;
 server.use(middlewares)
 server.use(jsonServer.rewriter({
   "/api/*": "/$1",
-  "/mysteries/:id": "/mysteries/:id",
+  "/mysteries/:id": "/mysteries?id=:id",
   "/mysteries/title/:title": "/mysteries?title=:title",
   "/mysteries/mission-id-name/:id/:name": "/mysteries?missions.:id.name=:name",
   "/mysteries/mission-id-date/:id/:date": "/mysteries?missions.:id.date=:date",
   "/mysteries/location/:location": "/mysteries?location=:location",
   "/mysteries/found/:found": "/mysteries?found=:found",
-  "/mysteries/mysterium/:mysterium": "/mysteries?q=:mysterium",
-  "/mysteries/explanation/:explanation": "/mysteries?q=:explanation"  
+  "/mysteries/mysterium/:mysterium": "/mysteries?mysterium_like=:mysterium",
+  "/mysteries/explanation/:explanation": "/mysteries?explanation_like=:explanation",
+  "/mysteries/:text": "/mysteries?q=:text"
 }))
 server.use(router)
 server.listen(port, () => {
